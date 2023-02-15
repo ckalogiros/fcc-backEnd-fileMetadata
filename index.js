810911cc6956f7b7704ab6c983e433471fddbcf1
ckalogiros <ckalogiros1@gmail.com>
2023-02-15 18:56:40 +0200

    third. Ready OK!

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer  = require('multer') // Multer middleware
+const upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
 app.use(cors());
+app.use(express.json())
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+/**
+ * in the 'upload.single('upfile'), the 'upfile' must match the 
+ *      '<input id="inputfield" type="file" name="upfile"> name''
+ * The req.file has the actual uploded file
+ * The req.body will contain the text fields, if there were any
+ */
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res)=>{
+    
+    const file = req.file;
+    const result = {
+        name:file.originalname,
+        type: file.mimetype,
+        size: file.size
+    }
+    console.log(file)
+    res.json(result);
+})
 
 
 const port = process.env.PORT || 3000;
